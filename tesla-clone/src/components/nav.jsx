3f1af359532abebfa29e9d6d4c9785b4fc02641f
balijawa hussein <balijawahussein@gmail.com>
2023-10-13 10:21:25 +0300

    nav bar

---
+++ tesla-clone/src/components/nav.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header className="nav-items">
+    <p><span>$7500 federal tax credit</span> available certain tesla models . Take delivery by Dec 31 for full $7,500 credit. Reductions likely for certain vehicles in 2024</p>
+      <h1>Logo</h1>
+      <nav className="nav-links">
+        <ul>
+          <li><a href="/">vehicle</a></li>
+          <li><a href="/about">energy</a></li>
+          <li><a href="/services">charging</a></li>
+          <li><a href="/contact">dicover</a></li>
+          <li><a href="/contact">shop</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
