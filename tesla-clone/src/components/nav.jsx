f4d3ffef33f94c53d2f1a784740cb95c158c8ebf
balijawa hussein <balijawahussein@gmail.com>
2023-10-16 22:35:03 +0300

    adding logo and svg

--- tesla-clone/src/components/nav.jsx
+++ tesla-clone/src/components/nav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Question from "./questionicon";
+import TeslaLogo from "./teslaLogo";
+import Web from "./webicon";
+import User from "./usericon";
   const Nav =()=>
   {
     return (
           <div className="nav-bar">
     <p><span>$7500 federal tax credit</span> available certain tesla models . Take delivery by Dec 31 for full $7,500 credit. Reductions likely for certain vehicles in 2024</p>
     <header className="nav-items">
-      <h1>Logo</h1>
+      <TeslaLogo />
       <nav className="nav-links">
         <ul>
           <li><button>vehicle</button></li>
@@ -14,6 +18,11 @@ import React from "react";
           <li><button>discover</button></li>
           <li><button>shop</button></li>
         </ul>
+        <div className="side-icons">
+          <Question />
+          <Web />
+          <User />
+        </div>
       </nav>
     </header>
     </div>
